805f63d13fb6006e822f308fa0e9bd19b2612b31
Kung-Hsiang <a86792511@gmail.com>
2021-04-13 15:50:15 +0800

    fix running slow issue

--- cleanlog.sh
+++ cleanlog.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-mv log/* olds/log
-mv output/* olds/output
-mv error/* olds/error
+echo "removing old_log"
+rm -r old_log 2> /dev/null
+echo "removing old_error"
+rm -r old_error 2> /dev/null
+echo "removing old_output"
+rm -r old_output 2> /dev/null
+echo "moving news to olds"
+mv -i log old_log
+mv -i output old_output
+mv -i error old_error
+echo "make folder error log output again"
+mkdir error log output
