7d0f4c8270b9f1d68975bb032a35e66c45708f04
Kong-Hsiang <a86792511@gmail.com>
2022-07-15 08:30:24 +0200

    fix wrong dir name

--- runAnalysis_getR_MC.sh
+++ runAnalysis_getR_MC.sh
@@ -14,7 +14,7 @@ inputfile=`echo $1|rev |cut -d '/' -f 1|rev`
 root -b -q ./GetRfunction_MC.C\(\"$inputfile\",\"$2\"\)
 echo "finish the code"
 if [ -e "$2" ]; then
-  until xrdcp -f "$2" /eos/user/k/kuchen/getR_QCD_MC/GetR_MC/"$2"; do
+  until xrdcp -f "$2" /eos/user/k/kuchen/getR_MC/"$2"; do
     sleep 60
     echo "Retrying"  
   done
